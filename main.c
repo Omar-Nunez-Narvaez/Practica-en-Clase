b11382ebf72647a90331a8741268ace6e02761e9
Laboratorio <laboratorio@udla-ec.int>
2026-05-08 15:09:02 -0500

    Agregar menú interactivo a la calculadora

--- main.c
+++ main.c
@@ -5,17 +5,55 @@
 #include "division.h"
 
 int main() {
+    int opcion;
     int a, b;
+    int resultado;
 
-    printf("Ingresar el 1er numero: ");
-    scanf("%d", &a);
-    printf("Ingresar el 2do numero: ");
-    scanf("%d", &b);
+    do {
+        
+        printf("\n=== CALCULADORA ===\n");
+        printf("1. Sumar\n");
+        printf("2. Restar\n");
+        printf("3. Multiplicar\n");
+        printf("4. Dividir\n");
+        printf("5. Salir\n");
+        printf("Elige una opcion: ");
+        scanf("%d", &opcion);
 
-    printf("La suma es: %d\n", sumar(a, b));
-    printf("La resta es: %d\n", restar(a, b));
-    printf("La multiplicacion es: %d\n", multiplicacion(a, b));
-    printf("La division es: %d\n", division(a, b));
+        if (opcion >= 1 && opcion <= 4) {
+            
+            printf("Ingresar el 1er numero: ");
+            scanf("%d", &a);
+            printf("Ingresar el 2do numero: ");
+            scanf("%d", &b);
+        }
+
+       
+        switch(opcion) {
+            case 1:
+                resultado = sumar(a, b);
+                printf("Resultado: %d\n", resultado);
+                break;
+            case 2:
+                resultado = restar(a, b);
+                printf("Resultado: %d\n", resultado);
+                break;
+            case 3:
+                resultado = multiplicacion(a, b);
+                printf("Resultado: %d\n", resultado);
+                break;
+            case 4:
+                resultado = division(a, b);
+                printf("Resultado: %d\n", resultado);
+                break;
+            case 5:
+                printf("Saliendo...\n");
+                break;
+            default:
+                printf("Opcion invalida!\n");
+        }
+
+    } while(opcion != 5);
 
     return 0;
 }
